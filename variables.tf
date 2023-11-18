8392d50a6fac34aff4b3759075c58c5b105c2e64
Aswin Raaj Rajasekaran <aswinraaj.rajasekaran@doverfs.com>
2023-11-18 23:48:26 +0530

    removed

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "aws_access_key" {
   description = "access key to be passed as variable"
-  default = "AKIATDBPTOPC2FTEQNUV"
 }
 
 variable "aws_secret_key" {
   description = "secret key to passed as variable"
-  default = "qKcA2hZ6ljNbZMPcTTl+CZyn+J5IIzowUvzS/+e5"
 }
 
 variable "location" {
